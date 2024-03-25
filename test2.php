3ee6542b1b54beeb33fc316ae91215ab322c6348
HaiderShanna <haidershanna@gmail.com>
2024-03-25 10:02:19 +0300

    git -m"test test"

--- test2.php
+++ test2.php
@@ -1,9 +1,11 @@
-<?php 
-
-?>
-
-
-<div class="parent">
-    <div class="child1">child1</div><br>
-    <div class="child2">child2</div><br>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>that's some text </h1>
+</body>
+</html>
